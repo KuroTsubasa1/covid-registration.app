4b3479d4d0cc3a32fc88377bd196ff9651f2ae6f
Lasse Harm <lharm@lynet.de>
2020-10-07 16:51:18 +0100

    Added checkout

--- src/LHarm/RegistrationAppBundle/Controller/FrontendController.php
+++ src/LHarm/RegistrationAppBundle/Controller/FrontendController.php
@@ -9,69 +9,113 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class FrontendController extends AbstractController
 {
+
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     /**
      * @param Request $request
+     *
      * @return Response
      *
      * Displays the apps landing page
      */
     public function index(Request $request)
     {
-       return $this->render('@LHarmRegistrationApp/landing.html.twig',
-       [
-           'title' => 'RegistrationApp',
-       ]);
+        return $this->render(
+            '@LHarmRegistrationApp/landing.html.twig',
+            [
+                'title' => 'RegistrationApp',
+                'local' => $request->getLocale(),
+            ]
+        );
     }
 
     /**
      * @param Request $request
+     *
      * @return Response
      *
      * Displays the apps guest form
      */
-    public function guestForm(Request $request, GuestRepository $guestRepository,TranslatorInterface $translator)
-    {
+    public function guestForm(
+        Request $request,
+        GuestRepository $guestRepository,
+        TranslatorInterface $translator
+    ) {
+        return $this->render(
+            '@LHarmRegistrationApp/form.html.twig',
+            [
+                'title' => 'RegistrationApp',
+            ]
+        );
+    }
 
-        if(mb_strtolower($request->getMethod())  == 'post')
+    public function checkIn(
+        Request $request,
+        GuestRepository $guestRepository,
+        TranslatorInterface $translator
+    ) {
+        if (mb_strtolower($request->getMethod()) == 'post')
         {
             $formData = $request->request->all();
 
-            $validationResult = $guestRepository->validation($translator,$formData);
+            $validationResult = $guestRepository->validation(
+                $translator,
+                $formData
+            );
 
-            if($validationResult === TRUE)
+            if ($validationResult === true)
             {
-                $guestRepository->save($translator,$formData);
+                $guestRepository->save($translator, $formData);
+                $this->session->set('sessionID', $this->session->getId());
 
-                return $this->render('@LHarmRegistrationApp/checkedIn.html.twig',
+                return $this->render(
+                    '@LHarmRegistrationApp/checkedIn.html.twig',
                     [
-                        'title' => 'RegistrationApp',
-                        'name' => $formData['form_name'],
-                        'street' => $formData['form_street_number'],
-                        'zip_code' => $formData['form_zip_city'],
+                        'title'        => 'RegistrationApp',
+                        'name'         => $formData['form_name'],
+                        'street'       => $formData['form_street_number'],
+                        'zip_code'     => $formData['form_zip_city'],
                         'phone_number' => $formData['form_phone'],
-                        'email' => $formData['form_email'],
-                        'note' => $formData['form_note'],
-                    ]);
+                        'email'        => $formData['form_email'],
+                        'note'         => $formData['form_note'],
+                    ]
+                );
             }
             else
             {
-                return $this->render('@LHarmRegistrationApp/form.html.twig',
+                return $this->render(
+                    '@LHarmRegistrationApp/form.html.twig',
                     [
                         'title' => 'RegistrationApp',
                         'error' => $validationResult,
-                    ]);
+                    ]
+                );
             }
         }
         else
         {
-
-            return $this->render('@LHarmRegistrationApp/form.html.twig',
+            return $this->render(
+                '@LHarmRegistrationApp/form.html.twig',
                 [
                     'title' => 'RegistrationApp',
-                ]);
+                ]
+            );
         }
+    }
+
+    public function checkOut(Request $request)
+    {
+        $this->session->invalidate();
+        return $this->redirectToRoute('guest_form');
     }
 }
---
+++ src/LHarm/RegistrationAppBundle/Resources/translations/messages.en.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'application.name' => 'COVID-19 Guest Registration',
+    'guest.form.headline.registration.form'=> 'Registration form for guests',
+    'guest.form.for-sure.name' => 'First name, last name',
+    'guest.form.street-number' => 'Street, house number',
+    'guest.form.zip-city' => 'Zip code, City',
+    'guest.form.email' => 'E-Mail',
+    'guest.form.phone' => 'Phone number',
+    'guest.form.notes' => 'Notes',
+    'guest.form.policy' => 'Check this custom checkbox',
+    'guest.form.checkin' => 'Check in',
+    'guest.form.checkout' => 'Check out',
+    'header.big.text' => 'Your company name',
+    'header.small.text' => 'Guiding text of the company',
+    'guest.form.headline.personal.data' => 'Your personal data',
+    'guest.form.validation.name' => 'Please enter a valid first and last name',
+    'guest.form.validation.street_number' => 'Please enter a valid street and house number',
+    'guest.form.validation.zip_city' => 'Please enter a valid postal code and city',
+    'guest.form.validation.email' => 'Please enter a valid e-mail address',
+    'guest.form.validation.phone' => 'Please enter a valid phone number',
+    'guest.form.validation.policy' => 'Please confirm that you have read and accept our privacy policy',
+
+];
